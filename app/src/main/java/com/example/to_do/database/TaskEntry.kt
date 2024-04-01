274507cc94729f4d0f4190cfc2eb0028f4e23864
geison <geisonbruno0@gmail.com>
2024-04-01 17:56:40 -0300

    feat: Created project estruture

---
+++ app/src/main/java/com/example/to_do/database/TaskEntry.kt
@@ -0,0 +1,16 @@
+package com.example.to_do.database
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+@Entity(tableName = "task_table")
+data class TaskEntry(
+    @PrimaryKey(autoGenerate= true)
+    var id: Int,
+    var tittle: String,
+    var priority: Int,
+    var timestamp: Long
+):Parcelable
