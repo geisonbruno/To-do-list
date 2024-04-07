14741807ee303bb1c5efbfe73fb3acaf813e4939
geisonbruno1 <geisonbruno0@gmail.com>
2024-04-07 20:47:29 -0300

    feat: Logica para deletar tarefa e deletar todas

--- app/src/main/java/com/example/to_do/ui/task/TaskFragment.kt
+++ app/src/main/java/com/example/to_do/ui/task/TaskFragment.kt
@@ -1,15 +1,22 @@
 package com.example.to_do.ui.task
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.example.to_do.R
 import com.example.to_do.databinding.FragmentTaskBinding
 import com.example.to_do.viewmodel.TaskViewModel
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -46,7 +53,57 @@ class TaskFragment : Fragment() {
             }
 
         }
+        ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0,
+        ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT){
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
 
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val taskEntry = adapter.currentList[position]
+                viewModel.delete(taskEntry)
+
+                Snackbar.make(binding.root, "Deleted!", Snackbar.LENGTH_LONG).apply {
+                    setAction("Undo") {
+                        viewModel.insert(taskEntry)
+                    }
+                    show()
+                }
+            }
+
+        }).attachToRecyclerView(binding.recyclerView)
+
+        setHasOptionsMenu(true)
         return binding.root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.task_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_delete_all -> deleteAllItem()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun deleteAllItem() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("Delete All")
+            .setMessage("Are you sure ?")
+            .setPositiveButton("Yes"){dialog, _ ->
+                viewModel.deleteAll()
+                dialog.dismiss()
+            }.setNegativeButton("No"){dialog, _ ->
+                dialog.dismiss()
+            }.create().show()
+    }
+
 }
